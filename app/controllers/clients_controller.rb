9ff69b203d00a97ca6ba22c10b9f65f8acf07011
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-04-19 11:38:51 +0700

    initial client

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,9 @@
+class ClientsController < ApplicationController
+	def index
+
+	end
+end
+
+class Item
+	attr_accessor :name
+end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :customers
+  resources :clients
   # Example resource route with options:
   #   resources :products do
   #     member do
