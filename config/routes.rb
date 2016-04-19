87cccc0288e2734e367332edbad71720c4277d58
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-04-19 15:39:37 +0700

    #5 user can login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -15,6 +16,7 @@ Rails.application.routes.draw do
   #   resources :products
 
   resources :users
+  resources :sessions
   resources :customers
   resources :clients
   # Example resource route with options:
