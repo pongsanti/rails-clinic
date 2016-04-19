757e57e6b8c4919844e0db7cb749dffff5e9fd28
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-04-19 13:19:47 +0700

    #3 encrypt password before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,13 @@ class User < ActiveRecord::Base
 	validates :email, uniqueness: true
 	validates :password, confirmation: true
 	validates :password_confirmation, presence: true
+
+	before_save :encrypt_password
+
+	def encrypt_password
+		if password.present?
+			self.password_salt = BCrypt::Engine.generate_salt
+			self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+		end
+	end
 end
