d56aa780b90b7b59b214b3f346658f405a40618e
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-04-29 20:54:10 +0700

    #36 continue leveraging devise

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'house#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
