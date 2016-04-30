fbdac5a58ba9370f75f271b9871242c35966fbee
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-04-30 22:52:14 +0700

    customer model test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,5 +4,16 @@ class Customer < ActiveRecord::Base
 
 	validates :prefix, presence: true
 	validates :name, presence: true
+  validates :surname, presence: true
+  validates :birthdate, presence: true
+
+  def age_in_days
+    age_in_days = nil
+
+    unless self.birthdate.nil?
+      age_in_days = (Time.now.to_date - self.birthdate).to_i
+    end 
+    age_in_days
+  end
 
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class CustomerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @customer = customers(:customer_david)
+  end
+
+  test "should not save without prefix" do
+    @customer.prefix = nil
+    assert_not @customer.save, "Saved the customer without title"
+  end
+
+  test "should not save without name" do
+    @customer.name = nil
+    assert_not @customer.save, "Saved the customer without name"
+  end
+
+  test "should not save without surname" do
+    @customer.surname = nil
+    assert_not @customer.save, "Saved the customer without surname"
+  end
+
+  test "should not save without birthdate" do
+    @customer.birthdate = nil
+    assert_not @customer.save, "Saved the customer without birthdate"
+  end
+
 end
