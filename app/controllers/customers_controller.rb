7ca72a6e54a283c7037e4bef276a9b9221a91f54
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-05-02 00:39:58 +0700

    #12 create customer list page

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,32 +2,58 @@ class CustomersController < ApplicationController
   
   before_action :authenticate_user!
 
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+  end
+
   def new
   	@customer = Customer.new
-    @prefixes = prefix_all
+    get_all_prefixes
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+    get_all_prefixes
   end
 
   def create
-    #render plain: customer_params.inspect
   	@customer = Customer.new(customer_params)
+    @customer.cn = '001'
   	
   	if @customer.save
-      render plain: customer_params.inspect
+      redirect_to action: 'index'
     else
-      @prefixes = prefix_all
+      get_all_prefixes
       render 'new'
     end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+ 
+    if @customer.update(customer_params)
+      redirect_to @customer
+    else
+      get_all_prefixes
+      render 'edit'
+    end    
+  end
+
+  def destroy
   end
 
   private
   	def customer_params
   		params.require(:customer).permit(:prefix_id, :name, :surname, :sex,
-  			:id_card, :passport_no, :birth_date,
+  			:birthdate, :id_card_no, :passport_no, :occupation,
   			:address, :sub_district, :district, :province, :postal_code,
   			:occupation, :tel_no)
   	end
 
-    def prefix_all
-      Prefix.all
+    def get_all_prefixes
+      @prefixes = Prefix.all
     end
 end
