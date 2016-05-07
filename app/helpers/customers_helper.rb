02549a4c637210d08679c400f2bf1d380c519b3c
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-05-07 16:00:03 +0700

    Display Customer age in show page

--- app/helpers/customers_helper.rb
+++ app/helpers/customers_helper.rb
@@ -1,2 +1,27 @@
 module CustomersHelper
+
+  def age_text(customer)
+    unless customer.birthdate.nil?
+      age_in_days = (Time.now.to_date - customer.birthdate).to_i
+      s = StringIO.new
+      s << year(age_in_days).to_s << " " << I18n.t('customers.show.years') << " "
+      s << month(age_in_days).to_s << " " << I18n.t('customers.show.months') << " "
+      s << day(age_in_days).to_s << " " << I18n.t('customers.show.days')
+      s.string
+    end
+  end
+
+  private
+    def year(age_in_days)
+      age_in_days / 365
+    end
+
+    def month(age_in_days)
+      (age_in_days % 365) / 30
+    end
+
+    def day(age_in_days)
+      (age_in_days % 365) % 30
+    end
+
 end
