f20d7815fbc296e0a25250ad5e5f3778b12335ad
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-07-30 23:06:43 +0700

    fix show customer error

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -4,11 +4,20 @@ class CustomersControllerTest < ActionController::TestCase
   # Including devise test helper
   include Devise::TestHelpers
 
-  test "should get index" do
+  setup do
     sign_in users(:john)
+    @customer = customers(:customer_david)
+  end
 
+  test "should get index" do
     get "index"
     assert_response :success
     assert_select "h3 small", I18n.t("customers.index.sub_text")
+  end
+
+  test "should get show" do
+    get "show", id: @customer.id
+
+    assert_response :success
   end
 end
