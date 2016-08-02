c7b62c554de1ef1e9ec7d92c1ef2bb80b79f6917
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-08-02 21:18:16 +0700

    customer model unit tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,6 +5,7 @@ class CustomerTest < ActiveSupport::TestCase
   ERR_PREFIX = "errors.messages."
   ERR_BLANK_KEY = ERR_PREFIX + "blank"
   ERR_INCLUSION = ERR_PREFIX + "inclusion"
+  ERR_INVALID = ERR_PREFIX + "invalid"
 
   def setup
     @customer = customers(:customer_david)
@@ -56,6 +57,42 @@ class CustomerTest < ActiveSupport::TestCase
     error_count 1
     error_contains error_msg(birthdate, ERR_BLANK_KEY)
   end
+
+  test "should not validate email if empty" do
+    @customer.email = nil
+    assert @customer.save, "Saved the customer without birthdate"
+    error_count 0
+  end
+
+  test "should validate email if present" do
+    email = "activerecord.attributes.customer.email"
+
+    @customer.email = "wrong email"
+    assert_not @customer.save
+    error_count 1
+    error_contains error_msg(email, ERR_INVALID)
+  end
+
+  test "should delete id_card masked input before save" do
+    @customer.id_card_no = "1-2345-00007-12-3"
+    assert @customer.save
+    @customer.reload
+    assert_equal "1234500007123", @customer.id_card_no
+  end
+
+  test "should delete home_phone masked input before save" do
+    @customer.home_phone_no = "0-1234-5678"
+    assert @customer.save
+    @customer.reload
+    assert_equal "012345678", @customer.home_phone_no
+  end
+
+  test "should delete tel_no masked input before save" do
+    @customer.tel_no = "08-9123-4567"
+    assert @customer.save
+    @customer.reload
+    assert_equal "0891234567", @customer.tel_no
+  end    
 
   test "should have prefix" do
     assert_equal prefixes(:mr).name, @customer.prefix.name
