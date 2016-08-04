6017f66b35ec57cc2c265ca28964f9c9c477e992
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-08-04 10:53:46 +0700

    Exam model unit tests

--- test/models/exam_test.rb
+++ test/models/exam_test.rb
@@ -23,6 +23,66 @@ class ExamTest < ActiveSupport::TestCase
     error_contains error_msg(weight, ERR_INVALID)
   end
 
+  test "should not validate height if empty" do
+    @exam.height = nil
+    assert @exam.save
+    error_count 0
+  end
+
+  test "should validate height if present" do
+    height = "activerecord.attributes.exam.height"
+
+    @exam.height = 1000
+    assert_not @exam.save
+    error_count 1
+    error_contains error_msg(height, ERR_INVALID)
+  end
+
+  test "should not validate pulse if empty" do
+    @exam.pulse = nil
+    assert @exam.save
+    error_count 0
+  end
+
+  test "should validate pulse if present" do
+    pulse = "activerecord.attributes.exam.pulse"
+
+    @exam.pulse = 1000
+    assert_not @exam.save
+    error_count 1
+    error_contains error_msg(pulse, ERR_INVALID)
+  end
+
+  test "should not validate bp_systolic if empty" do
+    @exam.bp_systolic = nil
+    assert @exam.save
+    error_count 0
+  end
+
+  test "should validate bp_systolic if present" do
+    bp_systolic = "activerecord.attributes.exam.bp_systolic"
+
+    @exam.bp_systolic = 1000
+    assert_not @exam.save
+    error_count 1
+    error_contains error_msg(bp_systolic, ERR_INVALID)
+  end
+
+  test "should not validate bp_diastolic if empty" do
+    @exam.bp_diastolic = nil
+    assert @exam.save
+    error_count 0
+  end
+
+  test "should validate bp_diastolic if present" do
+    bp_diastolic = "activerecord.attributes.exam.bp_diastolic"
+
+    @exam.bp_diastolic = 1000
+    assert_not @exam.save
+    error_count 1
+    error_contains error_msg(bp_diastolic, ERR_INVALID)
+  end
+
   private
     def error_count(count)
       assert_equal count, @exam.errors.count
