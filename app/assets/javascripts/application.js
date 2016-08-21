4806b57cf5db873d70268c57b19fbf5f3cb5bd65
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-08-21 23:51:11 +0700

    try fixing Sprockets::ArgumentError: require_tree argument must be a directory error on Heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,5 +17,5 @@
 //= require react_ujs
 //= require handlebars.runtime
 //= require bootstrap-sprockets
-//= require_tree ./templates/qs
+//= require_directory ./templates/qs
 //= require_directory .
