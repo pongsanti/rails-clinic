e1d2faaa39e41436682e3b56b8fe17e0495cae34
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-08-22 21:55:01 +0700

    create folder for third-party js files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,8 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require components
-//= require react_ujs
 //= require handlebars.runtime
 //= require bootstrap-sprockets
+//= require_directory ./third-party
 //= require_tree ./templates
 //= require_directory .
