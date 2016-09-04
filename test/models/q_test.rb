1ecbd45d68a3191977ea70d376d03e00a81a152d
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-04 20:46:17 +0700

    Queue model test

--- test/models/q_test.rb
+++ test/models/q_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should scope by category" do
+    assert_equal 2, Q.cat_is("W").count
+    assert_equal 1, Q.cat_is("C").count
+  end
+
 end
