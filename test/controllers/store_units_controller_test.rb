5e572ac03c62a6ac412f442f97c67389ca599e1b
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-06 11:02:01 +0700

    Store unit controller tests

--- test/controllers/store_units_controller_test.rb
+++ test/controllers/store_units_controller_test.rb
@@ -1,21 +1,47 @@
 require 'test_helper'
 
 class StoreUnitsControllerTest < ActionController::TestCase
+  # Including devise test helper
+  include Devise::TestHelpers
+
   setup do
+    sign_in users(:john)
     @store_unit = store_units(:one)
   end
 
   test "should get index" do
     get :index
+
     assert_response :success
-    assert_not_nil assigns(:store_units)
+    assert_assigns :q, :store_units
+  end
+
+  test "should get index with search found" do
+    get "index", q: {title_cont: "U"}
+
+    assert_response :success
+    assert_assigns :q, :store_units
+
+    assert_equal 1, a(:store_units).count
+  end
+
+  test "should get index with search not found" do
+    get "index", q: {title_cont: "999"}
+
+    assert_response :success
+    assert_assigns :q, :store_units
+
+    assert_equal 0, a(:store_units).count
   end
 
   test "should get new" do
     get :new
     assert_response :success
-  end
 
+    assert_assigns :q, :store_unit
+    assert a(:store_unit).new_record?
+  end
+=begin
   test "should create store_unit" do
     assert_difference('StoreUnit.count') do
       post :create, store_unit: { title: @store_unit.title }
@@ -46,4 +72,5 @@ class StoreUnitsControllerTest < ActionController::TestCase
 
     assert_redirected_to store_units_path
   end
+=end  
 end
