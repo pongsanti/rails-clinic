7d8d3ad1e2de53d55eb4eb0852aa1918891e4dcc
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-13 21:34:56 +0700

    comment out not relevant tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
+=begin
   test "should get new" do
     get :new
     assert_response :success
   end
-
+=end
 end
