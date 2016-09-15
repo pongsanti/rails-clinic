6b015db6ae6321b6cf02cad31262ddd83e03ead6
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-15 20:15:21 +0700

    select drug_usage 'Exam' drug helper

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -16,9 +16,26 @@ module ExamsHelper
     {}, class: "selectpicker form-control", data: {"live-search" => true }
   end
 
+  def select_drug_usage
+    select_tag "drug_usages", options_for_select( \
+      @drug_usages.map{ |d| [d.code, d.id, \
+      { data: {"subtext"=> d.text } } ] } \
+    ), \
+    class: "form-control", data: { 'live-search' => true }
+  end
+
+  def select_drug_usage_form(f)
+    f.select :drug_usage_id, options_for_select( \
+      @drug_usages.map{|d| [d.code, d.id, \
+      { data: {"subtext"=> d.text } } ] }, \
+      f.object.drug_usage_id \
+    ), \
+    {}, class: "selectpicker form-control", data: {"live-search" => true }
+  end
+
   private
     def select_drug_in_text(drug_in)
-      "#{drug_in.drug.name} Exp #{drug_in.expired_date}"
+      "#{drug_in.drug.name} (Exp: #{drug_in.expired_date})"
     end
 
     def select_drug_in_subtext(drug_in)
