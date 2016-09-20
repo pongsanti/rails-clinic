a362702185bd1ff37facd5398ee3d4ccfc43d753
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-20 15:15:36 +0700

    Add active column to 'Qs'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920064841) do
+ActiveRecord::Schema.define(version: 20160920081154) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -185,6 +185,7 @@ ActiveRecord::Schema.define(version: 20160920064841) do
     t.integer  "exam_id",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "active"
   end
 
   add_index "qs", ["exam_id"], name: "index_qs_on_exam_id", using: :btree
