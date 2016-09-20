8dc76d4ad568fbb6c536797ec436bc43f3b649ba
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-20 13:55:26 +0700

    'User' display_name migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160918124637) do
+ActiveRecord::Schema.define(version: 20160920064841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(version: 20160918124637) do
     t.datetime "updated_at",                          null: false
     t.integer  "client_id"
     t.string   "role"
+    t.string   "display_name"
   end
 
   add_index "users", ["client_id"], name: "index_users_on_client_id", using: :btree
