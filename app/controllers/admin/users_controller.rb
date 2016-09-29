edaa7c459212e5d76a401755c07bdfb139ed31ed
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-29 21:11:42 +0700

    admin 'User' index page

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,28 @@
+class Admin::UsersController < ApplicationController
+  
+  before_action :authenticate_user!
+  
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path, :notice => "Signed up!"
+    else
+      render "new"
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+
+end
