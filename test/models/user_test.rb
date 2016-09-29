76b0edd38c6555482c4513a0b666ac8144923def
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-09-29 21:42:44 +0700

    'User' model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
+require 'models/common'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include Common
+  
+  def setup
+    @user = users(:david)
+    @test_target = @user
+  end
+
+  test "should has 4 roles" do
+    assert 4, User::ROLES.count
+  end
+
+  test "should set role" do
+    role = User::ROLES[0]
+    @user.roles = [role]
+
+    assert_equal role, @user.roles[0]
+    assert @user.has_role? role
+  end
+
+  test "should set roles" do
+    roles = [User::ROLES[0], User::ROLES[3]]
+    @user.roles = roles
+
+    roles.each do |r|
+      assert @user.roles.include? r
+      assert @user.has_role? r
+    end
+  end
+
 end
