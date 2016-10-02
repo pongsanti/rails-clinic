6921d9b03ab05d198b60b48b93e2a521dc062f3e
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-03 04:06:30 +0700

    exclude heroku subdomain

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -90,3 +90,4 @@ end
 # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
 Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
 # Rails.application.config.middleware.use 'Apartment::Elevators::FirstSubdomain'
+Apartment::Elevators::Subdomain.excluded_subdomains = ['morning-river-86688']
