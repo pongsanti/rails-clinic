b4430ee9e85e497975842225bce7fee1d4e891a8
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-03 00:09:14 +0700

    'Client' subdomain

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161001051937) do
+ActiveRecord::Schema.define(version: 20161002170823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20161001051937) do
     t.string   "mobile_phone_no"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "subdomain"
   end
 
   create_table "customers", force: :cascade do |t|
