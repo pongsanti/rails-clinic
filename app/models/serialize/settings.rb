312ea35447f5ea30179c596441f522dbefb86663
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-05 20:32:35 +0700

    'Client' serialize setttings

---
+++ app/models/serialize/settings.rb
@@ -0,0 +1,21 @@
+class Serialize::Settings
+
+  include Virtus.model
+
+  attribute :drug_width, Integer, default: "46mm"
+  attribute :drug_height, Integer, default: "80mm"
+
+
+  class << self
+    
+    def dump(settings)
+      settings.to_hash
+    end
+
+    def load(settings)
+      new(settings)
+    end
+
+  end
+
+end
