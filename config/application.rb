78e1fc8269726fb89f53101c564ce1275e900d1b
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-05 20:12:41 +0700

    db structure dump as sql

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module Clinic
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # hstore
+    config.active_record.schema_format = :sql
   end
 end
