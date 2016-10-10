4d2fa3ceaef65f27465ab90fbdb658049802829e
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-10 10:38:07 +0700

    revise 'DrugIn' query method

--- app/models/drug_in.rb
+++ app/models/drug_in.rb
@@ -20,13 +20,7 @@ class DrugIn < ActiveRecord::Base
     def for_drug(drug_id)
       # workaround for ransack bug
       # https://github.com/activerecord-hackery/ransack/issues/593
-      drug_id = 1 if drug_id == true
-
       where("drug_id = ?", drug_id)
-    end
-
-    def ransackable_scopes(auth_object = nil)
-      %i(for_drug)
     end
   end
 
