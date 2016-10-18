e7d7bd1976fc89cf2c26c244cee0401fe81066e5
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-18 17:04:34 +0700

    update puma config file

--- config/puma.rb
+++ config/puma.rb
@@ -23,7 +23,7 @@ state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  #require "active_record"
+  #ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  #ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
 end
