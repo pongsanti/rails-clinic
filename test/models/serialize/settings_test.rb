85626816222869a670989c013c3cf533045dad51
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-19 09:22:10 +0700

    setting configuration hash tests

--- test/models/serialize/settings_test.rb
+++ test/models/serialize/settings_test.rb
@@ -16,26 +16,29 @@ class Serialize::SettingsTest < ActiveSupport::TestCase
     font_size = 16
     @settings.drug_font_size = font_size
 
-    assert_equal({
-      drug_font_size: font_size,
-      drug_width: "80mm",
-      drug_height: "46mm"
-      }, Serialize::Settings.dump(@settings))
+    assert_equal(default_val.merge(drug_font_size: font_size),
+      Serialize::Settings.dump(@settings))
   end
 
   test "should load" do
     font_size = 20
     drug_width = "20mm"
 
-    @settings = Serialize::Settings.load({
+    @settings = Serialize::Settings.load(default_val.merge(
       drug_font_size: font_size,
       drug_width: drug_width
-    })
+    ))
 
     assert_equal font_size, @settings.drug_font_size
     assert_equal drug_width, @settings.drug_width
     assert_equal "46mm", @settings.drug_height
 
-  end  
+  end
+
+  private
+    def default_val
+      { drug_font_size: 12, drug_width: "80mm", drug_height: "46mm",
+        appointment_font_size: 12, appointment_width: "80mm", appointment_height: "46mm" }
+    end
 
 end
