3ee7f91b1f080bd0781535384b3e2ba7e9f9f84d
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-19 17:37:01 +0700

    revise Puma config file

--- config/puma.rb
+++ config/puma.rb
@@ -4,6 +4,11 @@ workers 1
 # Min and Max threads per worker
 threads 1, 6
 
+#General rule is to use preload_app when your workers die often and need fast starts.
+#If you don’t have many workers, you probably should not use preload_app.
+
+#preload_app!
+
 app_dir = File.expand_path("../..", __FILE__)
 shared_dir = "#{app_dir}/shared"
 
@@ -14,6 +19,7 @@ environment rails_env
 # Set up socket location
 bind "unix://#{shared_dir}/sockets/puma.sock"
 
+
 # Logging
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
@@ -23,7 +29,11 @@ state_path "#{shared_dir}/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
-  #require "active_record"
-  #ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  #ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  ActiveSupport.on_load(:active_record) do
+    ActiveRecord::Base.establish_connection
+  end
+end
+
+before_fork do
+  ActiveRecord::Base.connection_pool.disconnect!
 end
