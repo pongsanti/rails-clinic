2676ad3ba9efa3da98b36a478e6835c90eabdd70
pongsanti <pongsanti.tanvejsilp@gmail.com>
2016-10-20 22:38:22 +0700

    manager can update users display name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:role)
+      params.require(:user).permit(:display_name, :role)
     end
 
     def set_ransack_search_param
